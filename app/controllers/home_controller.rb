a6ea12e0453618c984ca8d6e7dd36c72dc0040ce
AquisTech <anand.bait@gmail.com>
2019-11-29 20:50:23 +0530

    Clean old code from home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,34 +1,4 @@
 class HomeController < ApplicationController
   def index
-    @date = Date.parse(params[:date]) rescue Date.today
-    transaction = current_user.transactions.new
-    transaction.payments.build
-    @transactions = [transaction]
-    transfer = current_user.transfers.new
-    @transactions << transfer
-    occurrences = current_user.occurrences.for(@date)
-    # TODO: Keep new on top and filled ones below
-    occurrences.each do |occurrence|
-      tp = occurrence.transaction_purpose
-      if tp.transfer?
-        if tp.transfers.where(transacted_at: @date.all_day).present?
-          tp.transfers.where(transacted_at: @date.all_day).each { |t| @transactions << t }
-        else
-          transfer = tp.transfers.build(amount: tp.estimate)
-          @transactions << transfer
-        end
-      else
-        if tp.transactions.where(transacted_at: @date.all_day).present?
-          tp.transactions.where(transacted_at: @date.all_day).each { |t| @transactions << t }
-        else
-          transaction = tp.transactions.build(amount: tp.estimate)
-          transaction.payments.build(amount: tp.estimate)
-          @transactions << transaction # TODO: add scope in tp.transactions.for(Date.today || params[:date])
-        end
-      end
-    end
-    # TODO: Change above logic instead of looping only fetch collect of today's transaction and place them at bottom.
-    # @other_day_transaction_purposes = current_user.transaction_purposes.where.not(id: occurrences.pluck(:transaction_purpose_id))
-    @other_day_transaction_purposes = current_user.transaction_purposes # TODO : Show only transaction purposes which are not for today
   end
 end
