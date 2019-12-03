891825a072bff18ba8a2b23a9c1adeba51ac4659
AquisTech <anand.bait@gmail.com>
2019-12-03 16:43:04 +0530

    Add default value for existing timestamps

--- db/migrate/20191203042500_add_missing_timestamps.rb
+++ db/migrate/20191203042500_add_missing_timestamps.rb
@@ -1,7 +1,7 @@
 class AddMissingTimestamps < ActiveRecord::Migration[5.2]
   def change
-    add_timestamps :products
-    add_timestamps :orders
-    add_timestamps :order_items
+    add_timestamps :products, null: false, default: Time.zone.now
+    add_timestamps :orders, null: false, default: Time.zone.now
+    add_timestamps :order_items, null: false, default: Time.zone.now
   end
 end
