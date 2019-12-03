ed3e47c6c7afc4e179ff03affbd0325c6d342b00
AquisTech <anand.bait@gmail.com>
2019-12-03 20:23:59 +0530

    Build Order and order_items

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def index
     # @order = current_user.orders.draft.first || Order.new
     @order = Order.draft.first || Order.new
+    @order.order_items.build
     @products = Product.all
   end
 end
