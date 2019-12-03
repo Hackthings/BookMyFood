d8f14e11f9be24c0a5bcc2181316644e463e14a9
AquisTech <anand.bait@gmail.com>
2019-12-03 20:28:30 +0530

    Fix: syntax error in reject_if

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   enum status: { draft: 0, placed: 1, payment_confirmed: 2, preparing: 3, prepared: 4, delivered: 5 }
   has_many :order_items
   has_many :products, through: :order_items
-  accepts_nested_attributes_for :order_items, allow_destroy: true, reject_if: { |oi|
+  accepts_nested_attributes_for :order_items, allow_destroy: true, reject_if: proc { |oi|
     puts oi.inspect
     oi.quantity.zero?
   }
