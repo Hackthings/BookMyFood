86a3c9e3a24903be1b5a1f949611521c2dea79f3
AquisTech <anand.bait@gmail.com>
2019-12-03 21:03:01 +0530

    Create only user specific order

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
-    # @order = current_user.orders.draft.first || Order.new
-    @order = Order.draft.first || Order.new
+    @order = current_user.orders.draft.first || current_user.orders.new
     @order.order_items.build
     @products = Product.all
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,5 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :products, through: :order_items
   belongs_to :user
-  accepts_nested_attributes_for :order_items, allow_destroy: true, reject_if: proc { |oi|
-    puts oi.inspect
-    oi[:quantity] == '0'
-  }
+  accepts_nested_attributes_for :order_items, allow_destroy: true, reject_if: proc { |oi| oi[:quantity] == '0' }
 end
